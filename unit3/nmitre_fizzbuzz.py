7778d9d9bd4c3a20d538a0846069581984ba54b0
Nicole <nicolemitre@uagateway.org>
2016-10-27 23:25:40 +0000

    today I  did fizzbuzz

--- unit3/nmitre_fizzbuzz.py
+++ unit3/nmitre_fizzbuzz.py
@@ -1,1 +1,15 @@
-name = input("Enter a number from 1 through 15: ")
+nam = input("Enter a number to start with: ")
+end = input("Enter a number to end with: ")
+end = int(end)
+nam = int(nam)
+
+for n in range(nam -1, end):
+    n = n+ 1
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3  == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz") 
+    else:
+        print(n)
