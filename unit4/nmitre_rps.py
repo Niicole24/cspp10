c9257e8c69449649db07154f6fd45663367a417f
Nicole <nicolemitre@uagateway.org>
2016-11-29 17:16:35 +0000

    Today i was trying to fix my lab.

--- unit4/nmitre_rps.py
+++ unit4/nmitre_rps.py
@@ -2,7 +2,7 @@ import random
 
 
 def get_p1_move():
-    N = input("1 = Rock\n2 = Paper\n3 = Scissors\n Enter a Number from 1-3: ")
+    N = input("1 = Rock\n2 = Paper\n3 = Scissors\n Enter number from 1-3: ")
     if N == "1":
         return 'r'
     elif N == "2":
@@ -27,23 +27,23 @@ def get_rounds():
 
 def get_round_winner(p1smove,cmove):
     if p1smove == 'r' and cmove == 's':
-        return "Player"
+        return "The Player wins!"
     elif p1smove == 's' and cmove == 'p':
-        return "Player"
+        return "The Player wins!"
     elif p1smove == 'p' and cmove == 'r':
-        return "Player"
+        return "The Player wins!"
     elif p1smove == 'p' and cmove == "s":
-        return "Computer"
+        return "The Computer Won!"
     elif p1smove == 's' and cmove == "r":
-        return "Computer"
+        return "The Computer Won!"
     elif p1smove == 'r' and cmove == 'p':
-        return "Computer"
+        return "The Computer Won!"
     elif p1smove == "r" and cmove == "r":
-        return "Tie"
+        return "It was a Tie!"
     elif p1smove == 'p' and cmove == 'p':
-        return "Tie"
+        return "It was a Tie!"
     elif p1smove == 's' and cmove == 's':
-        return "Tie"
+        return "It was a Tie"
         
 
 def get_full_move(short_move):
@@ -61,11 +61,11 @@ def print_score(player_score, comp_score, tie):
     comp_score = int(comp_score)
     tie = int(tie)
     if player_score > comp_score:
-        print ("You win with {} poins".format(player_score))
+        print ("You win with {} points".format(player_score))
     elif comp_score > player_score:
         print ("You lost by " + (comp_score - player_score) + " point!!")
     elif comp_score == player_score:
-        print ("You tied with the computer")
+        print ("You are tied with the computer.")
 
 
 def rps():
@@ -80,7 +80,7 @@ def rps():
         print(winner)
         if winner == "Player":
             player_score = player_score + 1
-        elif winner == "Comp":
+        elif winner == "Computer":
             comp_score = comp_score + 1
         elif winner == "Tie":
             ties = ties + 1
