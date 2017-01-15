8dec47347851f40c8d742910c78b052f6ff15a08
Nicole <nicolemitre@uagateway.org>
2017-01-15 21:42:33 +0000

    Me and jessica did it

--- unit5/mp_list1.py
+++ unit5/mp_list1.py
@@ -1,9 +1,22 @@
+#   working with jessica 
 x_list = []
-while True:
+x = 1
+while x != 0:
     user_input = int(input("Enter any number: "))
-    x_list.insert(0,user_input)
+    
+    if user_input >= 1:
+        x_list.insert(0,user_input)
+    
+    if user_input < 0:
+        if (user_input * -1) in x_list:
+            x_list.remove(user_input * -1)
+    
+    if user_input == 0:
+        print("End")
+        
     print(x_list)
-
+    
+    
 
 
 
